4d4ac6c6f61a0dd12ffe6ac57f0565f0c1ea588c
MoMagdy <14momagdi@gmail.com>
2022-08-14 06:20:23 +0200

    Fix: Auto Increment generator is added to gateway serialNumber.

--- src/main/java/com/bm/internship/gateway/entity/Gateway.java
+++ src/main/java/com/bm/internship/gateway/entity/Gateway.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Data
@@ -15,6 +17,8 @@ import javax.persistence.Id;
 @Entity(name="gateway")
 public class Gateway {
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     @Column(name = "serial_number")
     private String serialNumber;
     @Column(length = 50, nullable = false)
